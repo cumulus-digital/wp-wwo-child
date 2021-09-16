80db64f173d66ed5b105ffb8fd6cc78e824fc2e3
Daniel Vena <deaconvena+github@gmail.com>
2021-09-16 14:22:13 -0400

    Hamburger background expands right to left

--- build/child_frontend.asset.php
+++ build/child_frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '6a389c4a825d3612efaf5d8a07551dcd');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '02b4f4859c0ba136e48c79f253bb1401');
