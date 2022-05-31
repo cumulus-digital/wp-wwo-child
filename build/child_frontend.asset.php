9e73a1fab202f903098383d7c680e20bbe33e957
Daniel Vena <deaconvena+github@gmail.com>
2022-05-31 15:52:28 -0400

    post meta styling

--- build/child_frontend.asset.php
+++ build/child_frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'd96f19367c669c58f9ef');
+<?php return array('dependencies' => array('jquery'), 'version' => '0855cb6dc2c64a0bc277');
