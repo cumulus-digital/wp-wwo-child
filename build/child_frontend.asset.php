8992312338a9375600a3f6bbb72f05598820dbc1
Daniel Vena <deaconvena@gmail.com>
2022-05-31 22:29:46 -0400

    improve display of sidebar and footer text

--- build/child_frontend.asset.php
+++ build/child_frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '0855cb6dc2c64a0bc277');
+<?php return array('dependencies' => array('jquery'), 'version' => '59d7589eb4e7b899db73');
