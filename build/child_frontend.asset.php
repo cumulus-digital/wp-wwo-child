bb89a0120517875d5141ff8990acd4f143bf55cb
Daniel Vena <deaconvena+github@gmail.com>
2022-11-16 17:33:14 -0500

    packages updated

--- build/child_frontend.asset.php
+++ build/child_frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '977ffb8c804b0a477c8a');
+<?php return array('dependencies' => array('jquery'), 'version' => '22c214dcfd8f37ef25f7');
